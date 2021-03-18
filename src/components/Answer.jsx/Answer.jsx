e9773c0d2846af0c248df1b8bb378e659d726b1a
galaxyunicorn42 <43255742+galaxyunicorn42@users.noreply.github.com>
2021-03-18 15:56:09 -0700

    trivia

--- src/components/Answer.jsx/Answer.jsx
+++
@@ -1,8 +0,0 @@
-import React, { Component, useState } from "react";
-
-export default function Answer (props){
-  const {choices} = (props)
-  return (<div>
-    {choices.map((choice) => <div> {choice} </div>)}
-</div>)
-}
